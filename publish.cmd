86f03a0e1f24d7dbb3526b11d2fadbf8d569d05e
Esun Kim <veblush@gmail.com>
2016-05-16 08:54:10 +0900

    Publish

--- publish.cmd
+++ publish.cmd
@@ -1,17 +1,39 @@
-SET TEMPDIR="..\jekyll_output"
+@Echo off
 
-REM Build pages
+SET SRCDIR=".\veblush.github.io"
+SET TEMPDIR="..\veblush.github.io_output"
+
+Echo Go into srcdir
+pushd %~dp0
+CD %SRCDIR%
+
+Echo Build pages
 rd /s/q %TEMPDIR%
 call jekyll build -d %TEMPDIR%
+IF ERRORLEVEL 1 GOTO ERR 
 
-REM Update site with built pages
+Echo Update site with built pages
 git checkout master
+IF ERRORLEVEL 1 GOTO ERR 
 robocopy %TEMPDIR% . /MIR /FFT /Z /XA:H /W:5 /xd ".git"
+IF ERRORLEVEL 8 GOTO ERR 
 rd /s/q %TEMPDIR%
 
-REM Commit updates
+Echo Commit updates
 git add --all
 git commit -m "Publish"
 
-REM Return to original branch
+Echo Return to original branch
 git checkout site
+IF ERRORLEVEL 1 GOTO ERR
+
+popd
+
+echo done
+GOTO END
+
+:ERR
+
+echo !ERROR!
+
+:END
